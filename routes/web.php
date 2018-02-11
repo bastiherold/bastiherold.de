8f3c5d7e1b6c280224674cd73455f27e0a3f946d
Sebastian Herold <bastiherold@bastiherold.de>
2018-02-11 10:12:16 +0100

    Changed Messages Routes

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ Route::get('/logout', 'Auth\LoginController@logout');
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/messages/', 'MessageController@index')->name('messages.index');
 Route::post('/messages/', 'MessageController@store')->name('messages.store');
 Route::get('/mail/contact/', 'HomeController@sendContactMail');
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 
-  Route::get('/', 'Admin\AdminController@index');
+  Route::get('/', 'Admin\AdminController@index')->name('admin.home');
+
+  Route::get('/messages', 'MessageController@index')->name('messages.index');
+  Route::get('/messages/show/{message}', 'MessageController@show')->name('messages.show');
 
 });
