fee92810c4a215f24781e1b5e5c118982acb49f3
Sebastian Herold <bastiherold@bastiherold.de>
2018-02-15 16:01:31 +0100

    Added notification, tasks, messages dropdown dummy

--- resources/views/dashboard/partials/header/header.blade.php
+++ resources/views/dashboard/partials/header/header.blade.php
@@ -25,16 +25,98 @@
   </ul>
 
   <ul class="nav navbar-nav ml-auto">
-    <li class="nav-item d-md-down-none">
-      <a class="nav-link" href="#"><i class="icon-bell"></i><span class="badge badge-pill badge-danger">5</span></a>
+    <li class="nav-item dropdown d-md-down-none">
+      
+      <a class="nav-link" data-toggle="dropdown" href="#" role="button">
+        <i class="icon-bell"></i><span class="badge badge-pill badge-primary">666</span>
+      </a>
+
+      <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg">
+        <div class="dropdown-header text-center">
+          <strong>You have x notifications</strong>
+        </div>
+        
+        <a href="#" class="dropdown-item">
+          <i class="icon-user-follow text-success"></i> Whatever
+        </a>
+      
+        <div class="dropdown-header text-center">
+          <strong>Home</strong>
+        </div>
+        <a href="#" class="dropdown-item">
+          <div class="text-uppercase mb-1">
+          <small><b>Lights</b></small>
+          </div>
+          <span class="progress progress-xs">
+            <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+          </span>
+          <small class="text-muted">1 / 4 active</small>
+        </a>
+
+      </div>
+    </li>
+  
+    <li class="nav-item dropdown d-md-down-none">
+      <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">
+        <i class="icon-list"></i><span class="badge badge-pill badge-warning">42</span>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg">
+        <div class="dropdown-header text-center">
+          <strong>You have X pending tasks</strong>
+        </div>
+        <a href="#" class="dropdown-item">
+          <div class="small mb-1">Upgrade Fridge.exe
+            <span class="float-right">
+              <strong>42%</strong>
+            </span>
+          </div>
+          <span class="progress progress-xs">
+            <div class="progress-bar bg-info" role="progressbar" style="width: 42%" aria-valuenow="42" aria-valuemin="0" aria-valuemax="100"></div>
+          </span>
+        </a>
+        <a href="#" class="dropdown-item text-center">
+            <strong>View all completed tasks</strong>
+        </a>
+      </div>
     </li>
   
     <li class="nav-item d-md-down-none">
-      <a class="nav-link" href="#"><i class="icon-list"></i></a>
-    </li>
-  
-    <li class="nav-item d-md-down-none">
-      <a class="nav-link" href="#"><i class="icon-location-pin"></i></a>
+      <a class="nav-link" href="#" data-toggle="dropdown" role="button">
+        <i class="icon-envelope-letter"></i>
+        <span class="badge badge-pill badge-info">1337</span>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg">
+        
+        <div class="dropdown-header text-center">
+          <strong>You have 1337 Messages</strong>
+        </div>
+
+        <a href="#" class="dropdown-item">
+          <div class="message">
+            <div class="py-3 mr-3 float-left">
+              <div class="avatar">
+                <img src="/dashboard/img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
+                <span class="avatar-status badge-success"></span>
+              </div>
+            </div>
+            <div>
+              <small class="text-muted">Johanna Doe</small>
+              <small class="text-muted float-right mt-1">Just Years now</small>
+            </div>
+            <div class="text-truncate font-weight-bold">
+              <span class="fa fa-exclamation text-danger"></span>
+              Unimportant message
+            </div>
+            <div class="small text-muted text-truncate">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...              
+            </div>
+          </div>
+        </a>
+        
+        <a href="#" class="dropdown-item text-center">
+          <strong>Throw new Exception ;)</strong>
+        </a>
+      </div>
     </li>
   
     <li class="nav-item dropdown">
