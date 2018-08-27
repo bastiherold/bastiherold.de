0537390484bc0dca031565a33469a04a018917a4
Sebastian Herold <bastiherold@bastiherold.de>
2018-08-27 16:16:12 +0200

    added VueJs fixed scrollTo

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -1,24 +1,26 @@
 $(function() {
   $(window).scroll(function() {
       $(this).scrollTop() > 100 ? $("#toTop").fadeIn() : $("#toTop").fadeOut()
-  }), $("#toTop").click(function() {
+  });
+  $("#toTop").click(function() {
       $("html, body").animate({
           scrollTop: 0
       }, 666)
-  }), $(".navbar-nav a, .breadcrumb a, .section-link-icon").bind("click", r), $(".contact-btn").click()
-  function r() {
-    var t = $(this).attr("href");
-    if (t == '#legal') {
-        $("#legal").fadeIn("fast");
-        $("html, body").animate({
-            scrollTop: $($(this).attr("href")).offset().top
-        }, 666);
+  });
+  function scrollToLink() {
+      var t = $(this).attr("href");
+      if (t == '#legal') {
+          $("#legal").fadeIn("fast");
+          $("html, body").animate({
+              scrollTop: $($(this).attr("href")).offset().top
+            }, 666);
+        }
+        if (t == '#privacy') {
+            $("#privacy").fadeIn("fast");
+            $("html, body").animate({
+                scrollTop: $($(this).attr("href")).offset().top
+            }, 666);
+        }
     }
-    if (t == '#privacy') {
-        $("#privacy").fadeIn("fast");
-        $("html, body").animate({
-            scrollTop: $($(this).attr("href")).offset().top
-        }, 666);
-    }
-  }
+    $(".navbar-nav a, .breadcrumb a, .section-link-icon").bind("click", scrollToLink);
 });
