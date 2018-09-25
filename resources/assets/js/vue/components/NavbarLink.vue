9519c1929868e9fa738b3b17a9ab68689369c6fc
Sebastian Herold <bastiherold@bastiherold.de>
2018-09-25 11:55:28 +0200

    added vuejs navbarcomp

---
+++ resources/assets/js/vue/components/NavbarLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <li :title="this.title">
+    <a :href="this.href">
+      <i :class="this.classes" aria-hidden="true"></i> {{ title }}
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    href: {
+      type: String,
+      required: true
+    },
+    classes: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
